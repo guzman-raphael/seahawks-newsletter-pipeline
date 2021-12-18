1230b46bdba425317c829c0a873dc1abdf618068
davidgodinez <davidgodinez70@gmail.com>
2021-12-18 07:09:59 -0600

    Update setup.py

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,15 @@ RUN apt update && apt -y install mysql-client-5.7 netcat
 
 ENV TEAM="Seahawks"
 
-#RUN apt update && apt -y install mysql-client-5.7 netcat
-
 RUN mkdir /seahawks
 
 WORKDIR /seahawks
 
 RUN git clone https://github.com/davidgodinez/seahawks-newsletter-pipeline.git /seahawks
 
-RUN pip install --upgrade pip && pip install . 
+RUN pip install --upgrade pip && pip install .
 
-RUN pip insall -r requirements.txt
+RUN pip install -r requirements.txt
 
 COPY ./dj_local_conf.json /seahawks/dj_local_conf.json
 
--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = [r.rstrip().split('#')[0].rstrip() for r in f]
+
+
 setup(
     name = 'pipeline',
-    packages = find_packages(),
+    version='0.0.1a',
+    description="Seahawks Pipeline",
+    long_description="Pipeline Project with Raphael",
+    author='David Godinez',
+    packages = find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=requirements
 )
